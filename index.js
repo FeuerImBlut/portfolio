23ae1335d2b43292abdc6a370e09aab991488f90
FeuerImBlut <golubovich.as@gmail.com>
2022-01-30 16:06:59 +0300

    feat: add some features

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+/*burger menu*/
 const burger = document.querySelector('.burger');
 const shadow = document.querySelector('.shadow');
 const nav = document.querySelector('.nav-top');
 const links = document.querySelectorAll('.nav-top li');
 
-[burger, shadow].forEach((element) => {
+[...links, burger, shadow].forEach((element) => {
     element.addEventListener('click', toggleOpen);
 });
-links.forEach((element) => {
-    element.addEventListener('click', toggleOpen);
-})
 
 function toggleOpen() {
-    burger.classList.toggle('open');
-    shadow.classList.toggle('open');
-    nav.classList.toggle('open');
+    [burger, shadow, nav].forEach((element) => {
+        element.classList.toggle('open');
+    });
 }
+
+/* photo gallery*/
+const buttons = document.querySelectorAll('.portfolio-buttons > button');
+const gallery = document.querySelectorAll('.portfolio-items > img');
+
+buttons.forEach((element) => {
+    element.addEventListener('click', () => { galleryUpdate(element) });
+});
+
+function galleryUpdate(element) {
+    for (let i = 0; i < gallery.length; i++) {
+        gallery[i].src = `assets/img/${element.innerHTML}/${i + 1}.jpg`;
+        buttons.forEach((button) => {
+            button.classList.remove('active');
+        });
+        element.classList.add('active');
+    }
+}
+    /*images preload */
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+function preload() {
+    const img = new Image();
+    seasons.forEach((element) => {
+        for (let i = 1; i <= 6; i++) {
+            img.src = `assets/img/${element}/${i}.jpg`;
+        }
+    });
+}
+
+/*internationalisation*/
+
---
+++ translation.js
@@ -0,0 +1,15 @@
+const i18nObj = {
+    'en': {
+        'nav-text-1' : 'Skills',
+        'nav-text-2' : 'Portfolio',
+        'nav-text-3' : 'Video',
+        'nav-text-4' : 'Price',
+        'nav-text-5' : 'Contacts',
+        'hero-title' : 'Alexa Rise',
+        'hero-text' : 'Save sincere emotions, romantic feelings and happy moments of life together with professional photographer Alexa Rise',
+        'hero-hire' : 'Hire me',
+        
+        
+
+    }
+}
